8d05e8e08a48beae7f99ba056369add29edb3f93
Anastasia <pristupa1998@mail.ru>
2020-11-20 11:17:08 +0300

    1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,14 @@ class PagesController < ApplicationController
         @category = Category.all
     end
     def show
-        @news = DataNews.where(id: params[:id])
+        @news = DataNews.find(params[:id])
     end
     def login
         redirect_to new_admin_user_session_path
     end
     private
     def news_params
-        params.require(:data_news).permit(:news_name,:news_desk, :image_news)
+        params.require(:data_news).permit(:news_name,:news_desk, :image_news, :category_id)
     end
     def new 
     end
--- db/migrate/20201111004701_create_data_news.rb
+++ db/migrate/20201111004701_create_data_news.rb
@@ -3,6 +3,7 @@ class CreateDataNews < ActiveRecord::Migration[6.0]
     create_table :data_news do |t|
       t.string :news_name
       t.string :news_desk
+      t.integer :category_id
       t.timestamps
       t.belongs_to :category
       #t.has_one_attached :image_news
